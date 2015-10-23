9b9fa2f9bf243fc27db9367caf555d680496031b
Will Whitney <wwhitney@mit.edu>
2015-10-23 19:32:08 -0400

    python doesn't allow dict access with .

--- cf_runner.py
+++ cf_runner.py
@@ -112,7 +112,7 @@ for job in jobs:
             slurmfile.write(jobcommand)
 
         if not dry_run:
-            if job.gpuid >= 0:
+            if job['gpuid'] >= 0:
                 os.system("sbatch -N 1 -c 1 --gres=gpu:1 -p gpu --time=6-23:00:00 slurm_scripts/" + jobname + ".slurm &")
             else:
                 os.system("sbatch -N 1 -c 2 --time=6-23:00:00 slurm_scripts/" + jobname + ".slurm &")
